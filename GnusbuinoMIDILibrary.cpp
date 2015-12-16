27b5619bf8a899a37d8bc53ac6b06e96c1d81da2
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-12-16 11:28:13 +0700

    sysex message to reset

--- GnusbuinoMIDILibrary.cpp
+++ GnusbuinoMIDILibrary.cpp
@@ -135,7 +135,7 @@ void usbFunctionWriteOut(unsigned char * data, unsigned char len)
 {
 	if (len == 4)
 	{
-		//00000000  07 F0 08 F7              . ð . ÷          
+		//Sysex message  07 F0 08 F7
 		if (	
 			data[0] == 0x07 && data[1] == 0xF0 && data[2] == 0x08 && data[3] == 0xF7
 			)
