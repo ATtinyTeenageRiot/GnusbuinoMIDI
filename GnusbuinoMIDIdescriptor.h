69a42a188ca3c7d1fae7f6bd69af31c65412e4e9
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-12-20 18:25:35 +0700

    fix midi not detected in android

--- GnusbuinoMIDIdescriptor.h
+++ GnusbuinoMIDIdescriptor.h
@@ -229,7 +229,7 @@ static PROGMEM const char configDescrMIDI[] = {	/* USB configuration descriptor 
     9,			/* bLenght */
     USBDESCR_ENDPOINT,	/* bDescriptorType = endpoint */
     1 | M10_DIR_OUT,	/* bEndpointAddress OUT endpoint number 1 */
-    M10_INTERRUPT,		/* bmAttributes: 2:Bulk, 3:Interrupt endpoint */
+    M10_BULK,		/* bmAttributes: 2:Bulk, 3:Interrupt endpoint */
     8, 0,			/* wMaxPacketSize */
     2,			/* bIntervall in ms */
     M10_UNUSED,		/* bRefresh */
@@ -248,7 +248,7 @@ static PROGMEM const char configDescrMIDI[] = {	/* USB configuration descriptor 
     9,			/* bLenght */
     USBDESCR_ENDPOINT,	/* bDescriptorType = endpoint */
     M10_DIR_IN | 1,		/* bEndpointAddress IN endpoint number 1 */
-    M10_INTERRUPT,		/* bmAttributes: 2: Bulk, 3: Interrupt endpoint */
+    M10_BULK,		/* bmAttributes: 2: Bulk, 3: Interrupt endpoint */
     8, 0,			/* wMaxPacketSize */
     2,			/* bIntervall in ms */
     M10_UNUSED,		/* bRefresh */
