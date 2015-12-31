53aa4f1d0f65591a1d3b44cffabbebe7cabaa95d
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-12-31 15:11:46 +0700

    clean up config

--- usbconfig.h
+++ usbconfig.h
@@ -11,6 +11,9 @@
 #ifndef __usbconfig_h_included__
 #define __usbconfig_h_included__
 
+
+#include <GnusbuinoMIDIConfig.h>        // custom config
+
 /*
 General Description:
 This file is an example configuration (with inline documentation) for the USB
@@ -25,75 +28,79 @@ section at the end of this file).
 */
 
 #ifdef GNUSBUINOMIDI_ENABLE_CUSTOM_USB_CFG
-#ifdef GNUSBUINOMIDI_USB_IOPORTNAME
-#define USB_CFG_IOPORTNAME      GNUSBUINOMIDI_USB_IOPORTNAME
-#else
-#define USB_CFG_IOPORTNAME      B
-#endif
 
-#define USB_CFG_DMINUS_BIT      GNUSBUINOMIDI_USB_DMINUS
-#define USB_CFG_DPLUS_BIT       GNUSBUINOMIDI_USB_DPLUS
+		#ifdef GNUSBUINOMIDI_USB_IOPORTNAME
+			#define USB_CFG_IOPORTNAME      GNUSBUINOMIDI_USB_IOPORTNAME
+		#else
+			#define USB_CFG_IOPORTNAME      B
+		#endif
 
-#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
-#define USB_CFG_CHECK_CRC       0
+		#define USB_CFG_DMINUS_BIT      GNUSBUINOMIDI_USB_DMINUS
+		#define USB_CFG_DPLUS_BIT       GNUSBUINOMIDI_USB_DPLUS
+
+		#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
+		#define USB_CFG_CHECK_CRC       0
+
 #else
+
 /* ---------------------------- Hardware Config ---------------------------- */
-#if defined(__AVR_ATtiny85__)
-#define USB_CFG_IOPORTNAME      B
-#define USB_CFG_DMINUS_BIT      5
-#define USB_CFG_DPLUS_BIT       3
-#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
-#define USB_CFG_CHECK_CRC       0
-//#define USB_CFG_PULLUP_IOPORTNAME   B
-//#define USB_CFG_PULLUP_BIT          0
-#else
-#define USB_CFG_IOPORTNAME      D
-/* This is the port where the USB bus is connected. When you configure it to
- * "B", the registers PORTB, PINB and DDRB will be used.
- */
-#define USB_CFG_DMINUS_BIT      3
-/* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
- * This may be any bit in the port.
- */
-#define USB_CFG_DPLUS_BIT       2
-/* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
- * This may be any bit in the port. Please note that D+ must also be connected
- * to interrupt pin INT0! [You can also use other interrupts, see section
- * "Optional MCU Description" below, or you can connect D- to the interrupt, as
- * it is required if you use the USB_COUNT_SOF feature. If you use D- for the
- * interrupt, the USB interrupt will also be triggered at Start-Of-Frame
- * markers every millisecond.]
- */
-#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
-/* Clock rate of the AVR in kHz. Legal values are 12000, 12800, 15000, 16000,
- * 16500, 18000 and 20000. The 12.8 MHz and 16.5 MHz versions of the code
- * require no crystal, they tolerate +/- 1% deviation from the nominal
- * frequency. All other rates require a precision of 2000 ppm and thus a
- * crystal!
- * Since F_CPU should be defined to your actual clock rate anyway, you should
- * not need to modify this setting.
- */
-#define USB_CFG_CHECK_CRC       0
-/* Define this to 1 if you want that the driver checks integrity of incoming
- * data packets (CRC checks). CRC checks cost quite a bit of code size and are
- * currently only available for 18 MHz crystal clock. You must choose
- * USB_CFG_CLOCK_KHZ = 18000 if you enable this option.
- */
+	
+	#if defined(__AVR_ATtiny85__)
+		#define USB_CFG_IOPORTNAME      B
+		#define USB_CFG_DMINUS_BIT      5
+		#define USB_CFG_DPLUS_BIT       3
+		#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
+		#define USB_CFG_CHECK_CRC       0
+		//#define USB_CFG_PULLUP_IOPORTNAME   B
+		//#define USB_CFG_PULLUP_BIT          0
+	#else
+		#define USB_CFG_IOPORTNAME      D
+		/* This is the port where the USB bus is connected. When you configure it to
+		 * "B", the registers PORTB, PINB and DDRB will be used.
+		 */
+		#define USB_CFG_DMINUS_BIT      3
+		/* This is the bit number in USB_CFG_IOPORT where the USB D- line is connected.
+		 * This may be any bit in the port.
+		 */
+		#define USB_CFG_DPLUS_BIT       2
+		/* This is the bit number in USB_CFG_IOPORT where the USB D+ line is connected.
+		 * This may be any bit in the port. Please note that D+ must also be connected
+		 * to interrupt pin INT0! [You can also use other interrupts, see section
+		 * "Optional MCU Description" below, or you can connect D- to the interrupt, as
+		 * it is required if you use the USB_COUNT_SOF feature. If you use D- for the
+		 * interrupt, the USB interrupt will also be triggered at Start-Of-Frame
+		 * markers every millisecond.]
+		 */
+		#define USB_CFG_CLOCK_KHZ       (F_CPU/1000)
+		/* Clock rate of the AVR in kHz. Legal values are 12000, 12800, 15000, 16000,
+		 * 16500, 18000 and 20000. The 12.8 MHz and 16.5 MHz versions of the code
+		 * require no crystal, they tolerate +/- 1% deviation from the nominal
+		 * frequency. All other rates require a precision of 2000 ppm and thus a
+		 * crystal!
+		 * Since F_CPU should be defined to your actual clock rate anyway, you should
+		 * not need to modify this setting.
+		 */
+		#define USB_CFG_CHECK_CRC       0
+		/* Define this to 1 if you want that the driver checks integrity of incoming
+		 * data packets (CRC checks). CRC checks cost quite a bit of code size and are
+		 * currently only available for 18 MHz crystal clock. You must choose
+		 * USB_CFG_CLOCK_KHZ = 18000 if you enable this option.
+		 */
 
-/* ----------------------- Optional Hardware Config ------------------------ */
+		/* ----------------------- Optional Hardware Config ------------------------ */
 
-// #define USB_CFG_PULLUP_IOPORTNAME   B
-/* If you connect the 1.5k pullup resistor from D- to a port pin instead of
- * V+, you can connect and disconnect the device from firmware by calling
- * the macros usbDeviceConnect() and usbDeviceDisconnect() (see usbdrv.h).
- * This constant defines the port on which the pullup resistor is connected.
- */
-//#define USB_CFG_PULLUP_BIT          2
-/* This constant defines the bit number in USB_CFG_PULLUP_IOPORT (defined
- * above) where the 1.5k pullup resistor is connected. See description
- * above for details.
- */
-#endif
+		// #define USB_CFG_PULLUP_IOPORTNAME   B
+		/* If you connect the 1.5k pullup resistor from D- to a port pin instead of
+		 * V+, you can connect and disconnect the device from firmware by calling
+		 * the macros usbDeviceConnect() and usbDeviceDisconnect() (see usbdrv.h).
+		 * This constant defines the port on which the pullup resistor is connected.
+		 */
+		//#define USB_CFG_PULLUP_BIT          2
+		/* This constant defines the bit number in USB_CFG_PULLUP_IOPORT (defined
+		 * above) where the 1.5k pullup resistor is connected. See description
+		 * above for details.
+		 */
+	#endif
 
 #endif
 /* --------------------------- Functional Range ---------------------------- */
